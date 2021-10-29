19d204e3d5ce02761d8751d4624838b967826d01
kel888 <chong1999@graduate.utm.mygit config user.name kel888>
2021-10-28 17:56:15 -0700

    your comment

--- exercise.py
+++ exercise.py
@@ -1,6 +1,6 @@
 inches_str =input("How many inches of rain has fallen")
-inches_int= input(inches_str) 
-volume =(inches_int/12)*43560
+inches_float= float(inches_str) 
+volume =(inches_float/12)*43560
 gallons =volume*7.48051945
 print(inches_int, 'in. rain on 1 acre is", gallons, "gallons")
 
