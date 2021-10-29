5b6755e6a2d4678bc3c37c3a61f25a54e2b65cc2
kel888 <chong1999@graduate.utm.mygit config user.name kel888>
2021-10-28 17:51:55 -0700

    print area only

---
+++ exercise.py
@@ -0,0 +1,7 @@
+inches_str =input("How many inches of rain has fallen")
+inches_int= input(inches_str) 
+volume =(inches_int/12)*43560
+gallons =volume*7.48051945
+print(inches_int, 'in. rain on 1 acre is", gallons, "gallons")
+
+
