e17878fe1ee619696baf606bce08a53d2cb2739d
kel888 <chong1999@graduate.utm.mygit config user.name kel888>
2021-11-18 09:28:10 -0800

    example7

---
+++ SMJE4383_AP /CH3/example7.py
@@ -0,0 +1,9 @@
+limit_str="input("raneg is 1 to input: ")
+limit_int =int(limit_str) 
+count_int=1 
+sum_int=0 
+
+while count_int<= limit_int:
+	sum_int=sum_int+count_int 
+	count_int = count_int+1 
+	average = float(sum
